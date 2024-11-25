0404014798b597b4ac9daf3a026fe924ab2c22ef
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-11-24 22:00:14 -0800

    css files imported

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import './assets/css/reset.css';
+import './assets/css/style.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
