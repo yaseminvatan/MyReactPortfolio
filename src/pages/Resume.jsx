9c0abdf370d57aa4f2796d65a9bed8da7fb00fe8
yaseminvatan <66827861+yaseminvatan@users.noreply.github.com>
2024-12-05 05:47:21 -0800

    resume link updated

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,14 +4,13 @@ function Resume() {
   return (
     <section>
       <h2>Resume</h2>
-      <a href="/path-to-resume.pdf" download>Download Resume</a>
-      <ul>
+      <a href="public/ Resume.pdf" download>Download Resume</a>
+      {/* <ul>
         <li>JavaScript</li>
         <li>React</li>
         <li>Node.js</li>
         <li>GIS tools</li>
-        {/* Add other proficiencies after bootcamp to update my portfolio */}
-      </ul>
+      </ul> */}
     </section>
   );
 }
